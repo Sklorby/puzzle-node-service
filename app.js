fd5a742f364b345ea918222a9987ec1a2ea828d0
Phelimon Sarpaning <32636728+PhelimonSarpaning@users.noreply.github.com>
2023-07-09 14:21:58 -0500

    update node service

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 const express = require('express');
-const prettier = require('prettier');
 const Redis = require('ioredis');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-      allowedHeaders: ["my-custom-header"],
-      credentials: true
-    }
-  });
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
+});
 const cors = require('cors');
 const { dataActions, setupPlayerByRoom, getPlayerSprites } = require('./db');
 const { client } = require('./mongo');
-const { addObject } = require('./utils');
+const { addObject, removeObject } = require('./utils');
 
 const PORT = 3001;
 
 // Database Name
-const dbName = 'puzzle_game';
 
 // Collection name
-const room_collection = 'room_values';
 
-
-let redisClient = new Redis();;
+let redisClient = new Redis();
 
 // Connect to the server
 client.connect((err) => {
@@ -38,15 +34,11 @@ client.connect((err) => {
   // Further operations will be performed here
 });
 
-const db = client.db(dbName);
-const collection = db.collection(room_collection);
-
-
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 const players = {};
@@ -62,106 +54,134 @@ io.on('connection', (socket) => {
   // }
   // dataActions(roomData, 'djjhsdhsjf')
 
-  socket.on('spriteRequest', (requestingPlayer, spriteName, respondingPlayer) => {
-    console.log(`Receieved request from ${requestingPlayer} to get sprite ${spriteName} from ${respondingPlayer}`);
-    
-    const socketId = players[respondingPlayer];
-    redisClient.get(respondingPlayer, (err, targetSocketId) => {
-      if (targetSocketId) {
-        console.log(`The socket id for responding player: ${respondingPlayer} is  ${targetSocketId}`);
-        io.to(targetSocketId).emit('spriteRequest', requestingPlayer, spriteName);
-      }
-    });
-  });
+  socket.on(
+    'spriteRequest',
+    (requestingPlayer, spriteName, respondingPlayer) => {
+      console.log(
+        `Receieved request from ${requestingPlayer} to get sprite ${spriteName} from ${respondingPlayer}`
+      );
+
+      redisClient.get(respondingPlayer, (err, targetSocketId) => {
+        if (targetSocketId) {
+          console.log(
+            `The socket id for responding player: ${respondingPlayer} is  ${targetSocketId}`
+          );
+          io.to(targetSocketId).emit(
+            'spriteRequest',
+            requestingPlayer,
+            spriteName
+          );
+        }
+      });
+    }
+  );
 
   socket.on('sendToRequestingPlayer', (requestingPlayer, playerObject) => {
     console.log(`Sending object to ${requestingPlayer}`);
 
     let name = `${requestingPlayer}-list`;
     redisClient.lpush(name, playerObject);
-    
-    const socketId = players[requestingPlayer];
+
     redisClient.get(requestingPlayer, (err, targetSocketId) => {
       if (targetSocketId) {
-        console.log(`Sending request to object to requesting player: ${requestingPlayer} with socketId ${targetSocketId}`);
+        console.log(
+          `Sending request to object to requesting player: ${requestingPlayer} with socketId ${targetSocketId}`
+        );
         io.to(targetSocketId).emit('exchangedSprites', playerObject);
       }
     });
   });
-  
 
   // Register a player with a socket ID and optional name
   socket.on('registerPlayer', (name) => {
     const playerId = socket.id;
     players[playerId] = { name };
-    redisClient.set(playerId, socket.id)
+    redisClient.set(playerId, socket.id);
     console.log(`Player ${playerId} registered with name "${name}".`);
   });
 
   // Listen for player values
-  socket.on('playerValues', (roomId, playerId, values) => {
-    console.log(`received values to room: ${roomId} from player ${playerId}:`, values);
-    
+  socket.on('playerValues', async (roomId, playerId, values, isRemove) => {
+    console.log(
+      `received values to room: ${roomId} from player ${playerId}:`,
+      values
+    );
+
     const roomData = {
       roomId,
       socketId: socket.id,
-      data: {[playerId] : values},
-      timestamp: new Date()
-    }
+      data: { [playerId]: values },
+      timestamp: new Date(),
+    };
 
     // Generate a unique key for the room data
     const key = `websocket_data:${roomId}`;
 
-
     const playerObject = values;
+    let res = [];
 
+    if (isRemove) {
+      console.log('About to remove sprite from redis');
 
+      // Check if the object exists in Redis cache
+      doesObjExistInRedis(key, async (exists) => {
+        if (exists) {
+          console.log('It exists for removal');
+          // Retrieve the object from Redis and perform operations
+          getObjFromRedis(key, async (retrievedObj) => {
+            const id = values?.id;
+            console.log('the object id', id);
+            res = await removeObject(retrievedObj, playerId, id);
+            console.log('emitting values to other players:', res);
+            socket.to(roomId).emit('playerValues', res);
+            // Store the modified object back in Redis
+            storeObjInRedis(key, res);
+          });
+        } else {
+          console.log('Nothing to remove', res);
+        }
+      });
+    } else {
+      // Check if the object exists in Redis cache
+      doesObjExistInRedis(key, async (exists) => {
+        if (exists) {
+          console.log('It exists');
+          // Retrieve the object from Redis and perform operations
+          getObjFromRedis(key, async (retrievedObj) => {
+            res = await addObject(retrievedObj, playerId, playerObject);
+            console.log('emitting values to other players:', res);
+            socket.to(roomId).emit('playerValues', res);
+            // Store the modified object back in Redis
+            storeObjInRedis(key, res);
+          });
+        } else {
+          res = await addObject([], playerId, playerObject);
+          console.log('emitting values to other players:', res);
+          socket.to(roomId).emit('playerValues', res);
+          // Store the modified object back in Redis
+          storeObjInRedis(key, res);
+        }
+      });
+    }
 
-    // Check if the object exists in Redis cache
-doesObjExistInRedis(key, (exists) => {
-  if (exists) {
-    // Retrieve the object from Redis and perform operations
-    getObjFromRedis(key, (retrievedObj) => {
-      const res = addObject(retrievedObj, playerId, playerObject);
-
-      // Store the modified object back in Redis
-      storeObjInRedis(key, res);
-    });
-  } else {
-
-    const res = addObject([], playerId, playerObject);
-    // Store the modified object back in Redis
-    storeObjInRedis(key, res);
-  }
-});
-
-    console.log("the key", key);
-
-    // Store the data in Redis cache for the room
-    redisClient.rpush(key, JSON.stringify(roomData?.data))
-               .then((response) => console.log(`Data stored in Redis for room ${roomId}:`, roomData?.data))
-               .catch((err) => console.error('Failed to store data in Redis:', err));
-
-    console.log("the rooom data", roomData);
-    dataActions(roomData, playerId, client)
-    // Broadcast the values to all other players in the same room as the sender
-    // socket.to(roomId).emit('playerValues', {[playerId] : values});
-    socket.to(roomId).emit('playerValues', values);
-    console.log(`emitting values to other players:`, {[playerId] : values});
+    console.log('the rooom data', roomData);
+    dataActions(roomData, playerId, client);
   });
 
   //list for update shape position
-  socket.on("updatedPlayerValues", (roomId, playerId, updatedShape) => {
-    console.log(`received updated position values to room: ${roomId} from player ${playerId}:`, updatedShape);
-    socket.broadcast.emit("updatedPlayerValues", playerId, updatedShape);
+  socket.on('updatedPlayerValues', (roomId, playerId, updatedShape) => {
+    console.log(
+      `received updated position values to room: ${roomId} from player ${playerId}:`,
+      updatedShape
+    );
+    socket.broadcast.emit('updatedPlayerValues', playerId, updatedShape);
   });
 
   // Listen for a player joining a room
   socket.on('joinRoom', async (roomId) => {
-
     // leave the current room if any
-    if(socket.roomId){
-      socket.leave(socket.roomId)
+    if (socket.roomId) {
+      socket.leave(socket.roomId);
     }
 
     //Join the new room
@@ -171,13 +191,13 @@ doesObjExistInRedis(key, (exists) => {
 
     redisClient.set(playerId, socket.id);
 
-    console.log(`player ${socket.id} with name ${players[socket.id].name} joined room ${roomId}`);
+    console.log(
+      `Adding ${players[socket.id].name} to room ${roomId} in mongoDB`
+    );
 
-    console.log(`Adding ${players[socket.id].name} to room ${roomId} in mongoDB`);
+    await setupPlayerByRoom(playerId, roomId);
 
-    await setupPlayerByRoom(playerId, roomId)
-
-    const newSpriteObject = await getPlayerSprites(roomId,playerId);
+    const newSpriteObject = await getPlayerSprites(roomId, playerId);
 
     console.log('Player added to room');
 
@@ -186,48 +206,7 @@ doesObjExistInRedis(key, (exists) => {
 
     console.log('About to send ne sprite object', newSpriteObject);
     io.to(socket.id).emit('newGameSprite', newSpriteObject);
-
-    // const clientsInRoom = await io.in(socket.id).allSockets();
-    // console.log('all the sockets', clientsInRoom);
-  
-    // // Retreive data from redis and emit to the room
-    // const key = `websocket_data:${roomId}`;
-
-    // redisClient.lrange(key, 0,-1).then((data) => {
-    //   if(data){
-    //     console.log("Getting data from redis", data);
-        
-    //     const playerMap = new Map();
-    //     data.forEach(element => {
-    //       const playerData = JSON.parse(element);
-    //       console.log("the meeeeeeee", playerData)
-    //       const keys = Object.keys(playerData);
-    //       const dataKey = keys?.[0];
-
-    //       const dataObject = playerData?.[dataKey]
-
-    //       if(playerMap.has(dataKey)){
-    //         dataObject?.map((element) => {
-    //           playerMap.get(dataKey).push(element);
-    //         })  
-    //       }else{
-    //         const playerDataArray = [];
-    //         dataObject?.map((element) => {
-    //           playerDataArray.push(element);
-    //         });  
-    //         playerMap.set(dataKey, playerDataArray);
-    //       }
-    //     });
-        
-    //     console.log("player data convertted", playerMap)
-        // io.to(roomId).emit('playerValues', Array.from(playerMap.entries()));
-    //   }
-    // })
-    // .catch((err) => {
-    //   console.error('Failed to retrieve data from Redis:', err);
-    // })
   });
-
 
   // Listen for a player leaving a room
   socket.on('leaveRoom', (roomId) => {
@@ -240,12 +219,11 @@ doesObjExistInRedis(key, (exists) => {
   });
 });
 
-
 // Function to retrieve the object from Redis cache
 function getObjFromRedis(key, callback) {
   redisClient.get(key, (error, objStr) => {
     if (error) {
-      console.error("Error retrieving object from Redis:", error);
+      console.error('Error retrieving object from Redis:', error);
       callback([]);
     } else {
       const obj = JSON.parse(objStr) || [];
@@ -254,24 +232,22 @@ function getObjFromRedis(key, callback) {
   });
 }
 
-
 // Function to store the object in Redis cache
 function storeObjInRedis(key, obj) {
   redisClient.set(key, JSON.stringify(obj), (error) => {
     if (error) {
-      console.error("Error storing object in Redis:", error);
+      console.error('Error storing object in Redis:', error);
     } else {
-      console.log("Object stored in Redis cache");
+      console.log('Object stored in Redis cache');
     }
   });
 }
-
 
 // Function to check if the object exists in Redis cache
 function doesObjExistInRedis(key, callback) {
   redisClient.exists(key, (error, exists) => {
     if (error) {
-      console.error("Error checking object existence in Redis:", error);
+      console.error('Error checking object existence in Redis:', error);
       callback(false);
     } else {
       callback(exists === 1);
--- utils.js
+++ utils.js
@@ -1,10 +1,8 @@
 // @ts-check
-const { exists,
-  client: redisClient,
-} = require('./redis');
+const { exists, client: redisClient } = require('./redis');
 const { shape_sprites } = require('./sprite');
 
-const getMessages = async (roomId = "0", offset = 0, size = 50) => {
+const getMessages = async (roomId = '0', offset = 0, size = 50) => {
   /**
    * Logic:
    * 1. Check if room with id exists
@@ -26,61 +24,57 @@ const getMessages = async (roomId = "0", offset = 0, size = 50) => {
   }
 };
 
-
 // Function to check if two objects are equal
 function areObjectsEqual(obj1, obj2) {
   return obj1?.name === obj2;
 }
 
 const setPlayerObjects = (roomSpritesIndex) => {
-let object = [];
-let nameObject = '';
-// Iterate over A and add one object to B if not already present
-for (let i = 0; i < shape_sprites.length; i++) {
-  const objA = shape_sprites[i];
-  let found = false;
+  let object = [];
+  let nameObject = '';
+  // Iterate over A and add one object to B if not already present
+  for (let i = 0; i < shape_sprites.length; i++) {
+    const objA = shape_sprites[i];
+    let found = false;
 
-  for (let j = 0; j < roomSpritesIndex.length; j++) {
-    const objB = roomSpritesIndex[j];
+    for (let j = 0; j < roomSpritesIndex.length; j++) {
+      const objB = roomSpritesIndex[j];
 
-    if (areObjectsEqual(objA, objB)) {
-      found = true;
-      break;
+      if (areObjectsEqual(objA, objB)) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      nameObject = objA?.name;
+      roomSpritesIndex.push(objA[nameObject]);
+      object = objA[nameObject];
+      break; // Stop iterating after adding one object to B
     }
   }
 
-  if (!found) {
-    nameObject = objA?.name;
-    roomSpritesIndex.push(objA[nameObject]);
-    object = objA[nameObject];
-    break; // Stop iterating after adding one object to B
-  }
-}
-
-console.log('th anem object', nameObject)
-return {
-  'randomSprite': object,
-  'name': nameObject
+  console.log('th anem object', nameObject);
+  return {
+    randomSprite: object,
+    name: nameObject,
+  };
 };
-}
 
 const getRandomSprites = () => {
-// Generate a random index within the range of A.length
-const randomIndex = Math.floor(Math.random() * shape_sprites?.length);
+  // Generate a random index within the range of A.length
+  const randomIndex = Math.floor(Math.random() * shape_sprites?.length);
 
-// Retrieve the random object from array A
-const randomObject = shape_sprites[randomIndex];
-const name  = randomObject?.name;
+  // Retrieve the random object from array A
+  const randomObject = shape_sprites[randomIndex];
+  const name = randomObject?.name;
 
-
-console.log('random index', randomObject[name])
-return {
-    'randomSprite': randomObject[name],
-    'name': name
+  console.log('random index', randomObject[name]);
+  return {
+    randomSprite: randomObject[name],
+    name: name,
   };
-}
-
-
+};
 
 function addObject(obj, playerName, object) {
   const playerIndex = obj.findIndex((item) => item.hasOwnProperty(playerName));
@@ -106,7 +100,13 @@ function addObject(obj, playerName, object) {
 }
 
 function removeObject(obj, playerName, objectId) {
-  const playerIndex = obj.findIndex((item) => item.hasOwnProperty(playerName));
+  console.log(
+    `The object I am removing from ${obj} with playerName: ${playerName} and object Id: ${objectId}`
+  );
+
+  const playerIndex = obj.findIndex((item) =>
+    Object.prototype.hasOwnProperty.call(item, playerName)
+  );
 
   if (playerIndex !== -1) {
     // Player exists, remove the object
@@ -121,21 +121,30 @@ function removeObject(obj, playerName, objectId) {
   return obj; // Return the modified obj array
 }
 
+const obj = [
+  {
+    Jake: [
+      { id: 1, name: 'Object 1' },
+      { id: 2, name: 'Object 2' },
+    ],
+  },
+  {
+    John: [
+      { id: 3, name: 'Object 3' },
+      { id: 4, name: 'Object 4' },
+    ],
+  },
+];
 
-// const obj = [
-//   { Jake: [{ id: 1, name: "Object 1" }, { id: 2, name: "Object 2" }] },
-//   { John: [{ id: 3, name: "Object 3" }, { id: 4, name: "Object 4" }] }
-// ];
+const newObject = { id: 3, name: 'Object 6' };
+const res = removeObject(obj, 'John', 3);
 
-// const newObject  = { id:3, name: "Object 6" }
-// const res = removeObject(obj, 'John', 3);
-
-// console.log('the rse', JSON.stringify(res));
+console.log('the rse', JSON.stringify(res));
 
 module.exports = {
   addObject,
   removeObject,
   getMessages,
   getRandomSprites,
-  setPlayerObjects
+  setPlayerObjects,
 };
