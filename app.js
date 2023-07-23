f45d4d33ab019a2dab0c922bc06f093321df1b1c
Phelimon Sarpaning <32636728+PhelimonSarpaning@users.noreply.github.com>
2023-07-23 16:37:08 -0500

    update url

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://research-puzzle.vercel.app',
     methods: ['GET', 'POST'],
     allowedHeaders: ['my-custom-header'],
     credentials: true,
@@ -34,8 +34,9 @@ client.connect((err) => {
   // Further operations will be performed here
 });
 
+// http://localhost:3000
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://research-puzzle.vercel.app',
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
