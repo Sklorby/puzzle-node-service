6808aaa2282aed16f7081033d6de2e6b8efd1f5e
Phelimon Sarpaning <32636728+PhelimonSarpaning@users.noreply.github.com>
2024-01-31 19:52:27 -0600

    Update secret info

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const {
 
 // Connection URL
 const url =
-  'mongodb+srv://sarpap:1inamillion@cluster0.9uq0rgf.mongodb.net/puzzle_game?retryWrites=true&w=majority';
+  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.9uq0rgf.mongodb.net/puzzle_game?retryWrites=true&w=majority`;
 
 const client = new MongoClient(url, { useUnifiedTopology: true });
 const db = client.db(dbName);
