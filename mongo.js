e2914b4ca35a2d04f4526f09528e5224de500a39
Miguel Cuellar <cuellar.mc@outlook.com>
2025-01-20 16:22:04 -0600

    Changed URL to new structure

--- mongo.js
+++ mongo.js
@@ -1,7 +1,11 @@
 const { MongoClient } = require('mongodb');
 
 // Connection URL
-const url = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vgw8s.mongodb.net/puzzle_game?retryWrites=true&w=majority`;
+//const url = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vgw8s.mongodb.net/puzzle_game?retryWrites=true&w=majority`;
+
+//updated connection URL
+const url = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vgw8s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // local test
 // const url =
